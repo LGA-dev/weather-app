d07a8a5c62ed7dbfd1fd245b80827d409f1e84b1
Luciano Alcaraz <LGA-dev@hotmail.com>
2020-10-24 20:22:09 -0300

    Add next five days forecast section

--- app/js/main.js
+++ app/js/main.js
@@ -21,25 +21,48 @@ window.addEventListener('load', ()=> {
 
   let weatherForecastHourly1Name = document.querySelector(".weather-forecast__card--hour-1--name");
   let weatherForecastHourly1Temperatures = document.querySelector(".weather-forecast__card--hour-1--temps");
-
   let weatherForecastHourly4Name = document.querySelector(".weather-forecast__card--hour-4--name");
   let weatherForecastHourly4Temperatures = document.querySelector(".weather-forecast__card--hour-4--temps");
-
   let weatherForecastHourly7Name = document.querySelector(".weather-forecast__card--hour-7--name");
   let weatherForecastHourly7Temperatures = document.querySelector(".weather-forecast__card--hour-7--temps");
-
   let weatherForecastHourly10Name = document.querySelector(".weather-forecast__card--hour-10--name");
   let weatherForecastHourly10Temperatures = document.querySelector(".weather-forecast__card--hour-10--temps");
-
   let weatherForecastHourly13Name = document.querySelector(".weather-forecast__card--hour-13--name");
   let weatherForecastHourly13Temperatures = document.querySelector(".weather-forecast__card--hour-13--temps");
-
   let weatherForecastHourly16Name = document.querySelector(".weather-forecast__card--hour-16--name");
   let weatherForecastHourly16Temperatures = document.querySelector(".weather-forecast__card--hour-16--temps");
-
   let weatherForecastHourly19Name = document.querySelector(".weather-forecast__card--hour-19--name");
   let weatherForecastHourly19Temperatures = document.querySelector(".weather-forecast__card--hour-19--temps");
 
+  let weatherFiveDaysForecastFirstDayName = document.querySelector(".weather-five-days-forecast__card--first-day--name-and-date");
+  let weatherFiveDaysForecastFirstDayMaxTemperature = document.querySelector(".weather-five-days-forecast__card--first-day--max-temp");
+  let weatherFiveDaysForecastFirstDayMinTemperature = document.querySelector(".weather-five-days-forecast__card--first-day--min-temp");
+  let weatherFiveDaysForecastFirstDayWindSpeed = document.querySelector(".weather-five-days-forecast__card--first-day--wind-speed");
+  let weatherFiveDaysForecastFirstDayRainChance = document.querySelector(".weather-five-days-forecast__card--first-day--rain-chance");
+
+  let weatherFiveDaysForecastSecondDayName = document.querySelector(".weather-five-days-forecast__card--second-day--name-and-date");
+  let weatherFiveDaysForecastSecondDayMaxTemperature = document.querySelector(".weather-five-days-forecast__card--second-day--max-temp");
+  let weatherFiveDaysForecastSecondDayMinTemperature = document.querySelector(".weather-five-days-forecast__card--second-day--min-temp");
+  let weatherFiveDaysForecastSecondDayWindSpeed = document.querySelector(".weather-five-days-forecast__card--second-day--wind-speed");
+  let weatherFiveDaysForecastSecondDayRainChance = document.querySelector(".weather-five-days-forecast__card--second-day--rain-chance");
+
+  let weatherFiveDaysForecastThirdDayName = document.querySelector(".weather-five-days-forecast__card--third-day--name-and-date");
+  let weatherFiveDaysForecastThirdDayMaxTemperature = document.querySelector(".weather-five-days-forecast__card--third-day--max-temp");
+  let weatherFiveDaysForecastThirdDayMinTemperature = document.querySelector(".weather-five-days-forecast__card--third-day--min-temp");
+  let weatherFiveDaysForecastThirdDayWindSpeed = document.querySelector(".weather-five-days-forecast__card--third-day--wind-speed");
+  let weatherFiveDaysForecastThirdDayRainChance = document.querySelector(".weather-five-days-forecast__card--third-day--rain-chance");
+  
+  let weatherFiveDaysForecastFourthDayName = document.querySelector(".weather-five-days-forecast__card--fourth-day--name-and-date");
+  let weatherFiveDaysForecastFourthDayMaxTemperature = document.querySelector(".weather-five-days-forecast__card--fourth-day--max-temp");
+  let weatherFiveDaysForecastFourthDayMinTemperature = document.querySelector(".weather-five-days-forecast__card--fourth-day--min-temp");
+  let weatherFiveDaysForecastFourthDayWindSpeed = document.querySelector(".weather-five-days-forecast__card--fourth-day--wind-speed");
+  let weatherFiveDaysForecastFourthDayRainChance = document.querySelector(".weather-five-days-forecast__card--fourth-day--rain-chance");
+
+  let weatherFiveDaysForecastFifthDayName = document.querySelector(".weather-five-days-forecast__card--fifth-day--name-and-date");
+  let weatherFiveDaysForecastFifthDayMaxTemperature = document.querySelector(".weather-five-days-forecast__card--fifth-day--max-temp");
+  let weatherFiveDaysForecastFifthDayMinTemperature = document.querySelector(".weather-five-days-forecast__card--fifth-day--min-temp");
+  let weatherFiveDaysForecastFifthDayWindSpeed = document.querySelector(".weather-five-days-forecast__card--fifth-day--wind-speed");
+  let weatherFiveDaysForecastFifthDayRainChance = document.querySelector(".weather-five-days-forecast__card--fifth-day--rain-chance");
 
   if(navigator.geolocation){
     // Get the current position of the device
@@ -69,6 +92,7 @@ window.addEventListener('load', ()=> {
           const dailyRainCh = data.daily[0].pop;
           const dailySunriseTm = convertUnixTimeStampToHourAndMinute(data.current.sunrise);
           const dailySunsetTm = convertUnixTimeStampToHourAndMinute(data.current.sunset);
+
           const weatherForecastDay1Nm = convertUnixTimeStampToDay(data.daily[1].dt);
           const weatherForecastDay1MinTemps = data.daily[1].temp.min;
           const weatherForecastDay1MaxTemps = data.daily[1].temp.max;
@@ -81,26 +105,50 @@ window.addEventListener('load', ()=> {
 
           const weatherForecastHourly1Nm = convertUnixTimeStampToHourAndMinute(data.hourly[1].dt);
           const weatherForecastHourly1Temps = data.hourly[1].temp;
-
           const weatherForecastHourly4Nm = convertUnixTimeStampToHourAndMinute(data.hourly[4].dt);
           const weatherForecastHourly4Temps = data.hourly[4].temp;
-          
           const weatherForecastHourly7Nm = convertUnixTimeStampToHourAndMinute(data.hourly[7].dt);
           const weatherForecastHourly7Temps = data.hourly[7].temp;
-
           const weatherForecastHourly10Nm = convertUnixTimeStampToHourAndMinute(data.hourly[10].dt);
           const weatherForecastHourly10Temps = data.hourly[10].temp;
-
           const weatherForecastHourly13Nm = convertUnixTimeStampToHourAndMinute(data.hourly[13].dt);
           const weatherForecastHourly13Temps = data.hourly[13].temp;
-
           const weatherForecastHourly16Nm = convertUnixTimeStampToHourAndMinute(data.hourly[16].dt);
           const weatherForecastHourly16Temps = data.hourly[16].temp;
-
           const weatherForecastHourly19Nm = convertUnixTimeStampToHourAndMinute(data.hourly[19].dt);
           const weatherForecastHourly19Temps = data.hourly[19].temp;
 
-          // const rain = data.rain['1h'];
+          const weatherFiveDaysForecastFirstDayNm = convertUnixTimeStampToDayMonth(data.daily[1].dt);
+          const weatherFiveDaysForecastFirstDayMaxTemp = data.daily[1].temp.max;
+          const weatherFiveDaysForecastFirstDayMinTemp = data.daily[1].temp.min;
+          const weatherFiveDaysForecastFirstDayWindSpd = data.daily[1].wind_speed;
+          const weatherFiveDaysForecastFirstDayRainCh = data.daily[1].pop;
+          
+          const weatherFiveDaysForecastSecondDayNm = convertUnixTimeStampToDayMonth(data.daily[2].dt);
+          const weatherFiveDaysForecastSecondDayMaxTemp = data.daily[2].temp.max;
+          const weatherFiveDaysForecastSecondDayMinTemp = data.daily[2].temp.min;
+          const weatherFiveDaysForecastSecondDayWindSpd = data.daily[2].wind_speed;
+          const weatherFiveDaysForecastSecondDayRainCh = data.daily[2].pop;
+
+          const weatherFiveDaysForecastThirdDayNm = convertUnixTimeStampToDayMonth(data.daily[3].dt);
+          const weatherFiveDaysForecastThirdDayMaxTemp = data.daily[3].temp.max;
+          const weatherFiveDaysForecastThirdDayMinTemp = data.daily[3].temp.min;
+          const weatherFiveDaysForecastThirdDayWindSpd = data.daily[3].wind_speed;
+          const weatherFiveDaysForecastThirdDayRainCh = data.daily[3].pop;
+
+          const weatherFiveDaysForecastFourthDayNm = convertUnixTimeStampToDayMonth(data.daily[4].dt);
+          const weatherFiveDaysForecastFourthDayMaxTemp = data.daily[4].temp.max;
+          const weatherFiveDaysForecastFourthDayMinTemp = data.daily[4].temp.min;
+          const weatherFiveDaysForecastFourthDayWindSpd = data.daily[4].wind_speed;
+          const weatherFiveDaysForecastFourthDayRainCh = data.daily[4].pop;
+
+          const weatherFiveDaysForecastFifthDayNm = convertUnixTimeStampToDayMonth(data.daily[5].dt);
+          const weatherFiveDaysForecastFifthDayMaxTemp = data.daily[5].temp.max;
+          const weatherFiveDaysForecastFifthDayMinTemp = data.daily[5].temp.min;
+          const weatherFiveDaysForecastFifthDayWindSpd = data.daily[5].wind_speed;
+          const weatherFiveDaysForecastFifthDayRainCh = data.daily[5].pop;
+
+          // const rain = data.rain['1h']; 
           // console.log(rain)
 
           // Display stored data to app
@@ -123,24 +171,48 @@ window.addEventListener('load', ()=> {
 
           weatherForecastHourly1Name.textContent = weatherForecastHourly1Nm;
           weatherForecastHourly1Temperatures.textContent = weatherForecastHourly1Temps;
-
           weatherForecastHourly4Name.textContent = weatherForecastHourly4Nm;
           weatherForecastHourly4Temperatures.textContent = weatherForecastHourly4Temps;
-
           weatherForecastHourly7Name.textContent = weatherForecastHourly7Nm;
           weatherForecastHourly7Temperatures.textContent = weatherForecastHourly7Temps;
-
           weatherForecastHourly10Name.textContent = weatherForecastHourly10Nm;
           weatherForecastHourly10Temperatures.textContent = weatherForecastHourly10Temps;
-
           weatherForecastHourly13Name.textContent = weatherForecastHourly13Nm;
           weatherForecastHourly13Temperatures.textContent = weatherForecastHourly13Temps;
-
           weatherForecastHourly16Name.textContent = weatherForecastHourly16Nm;
           weatherForecastHourly16Temperatures.textContent = weatherForecastHourly16Temps;
-
           weatherForecastHourly19Name.textContent = weatherForecastHourly19Nm;
           weatherForecastHourly19Temperatures.textContent = weatherForecastHourly19Temps;
+
+          weatherFiveDaysForecastFirstDayName.textContent = weatherFiveDaysForecastFirstDayNm;
+          weatherFiveDaysForecastFirstDayMaxTemperature.textContent = weatherFiveDaysForecastFirstDayMaxTemp;
+          weatherFiveDaysForecastFirstDayMinTemperature.textContent = weatherFiveDaysForecastFirstDayMinTemp;
+          weatherFiveDaysForecastFirstDayWindSpeed.textContent = weatherFiveDaysForecastFirstDayWindSpd;
+          weatherFiveDaysForecastFirstDayRainChance.textContent = (weatherFiveDaysForecastFirstDayRainCh*100);
+
+          weatherFiveDaysForecastSecondDayName.textContent = weatherFiveDaysForecastSecondDayNm;
+          weatherFiveDaysForecastSecondDayMaxTemperature.textContent = weatherFiveDaysForecastSecondDayMaxTemp;
+          weatherFiveDaysForecastSecondDayMinTemperature.textContent = weatherFiveDaysForecastSecondDayMinTemp;
+          weatherFiveDaysForecastSecondDayWindSpeed.textContent = weatherFiveDaysForecastSecondDayWindSpd;
+          weatherFiveDaysForecastSecondDayRainChance.textContent = (weatherFiveDaysForecastSecondDayRainCh*100);
+
+          weatherFiveDaysForecastThirdDayName.textContent = weatherFiveDaysForecastThirdDayNm;
+          weatherFiveDaysForecastThirdDayMaxTemperature.textContent = weatherFiveDaysForecastThirdDayMaxTemp;
+          weatherFiveDaysForecastThirdDayMinTemperature.textContent = weatherFiveDaysForecastThirdDayMinTemp;
+          weatherFiveDaysForecastThirdDayWindSpeed.textContent = weatherFiveDaysForecastThirdDayWindSpd;
+          weatherFiveDaysForecastThirdDayRainChance.textContent = (weatherFiveDaysForecastThirdDayRainCh*100);
+
+          weatherFiveDaysForecastFourthDayName.textContent = weatherFiveDaysForecastFourthDayNm;
+          weatherFiveDaysForecastFourthDayMaxTemperature.textContent = weatherFiveDaysForecastFourthDayMaxTemp;
+          weatherFiveDaysForecastFourthDayMinTemperature.textContent = weatherFiveDaysForecastFourthDayMinTemp;
+          weatherFiveDaysForecastFourthDayWindSpeed.textContent = weatherFiveDaysForecastFourthDayWindSpd;
+          weatherFiveDaysForecastFourthDayRainChance.textContent = (weatherFiveDaysForecastFourthDayRainCh*100);
+
+          weatherFiveDaysForecastFifthDayName.textContent = weatherFiveDaysForecastFifthDayNm;
+          weatherFiveDaysForecastFifthDayMaxTemperature.textContent = weatherFiveDaysForecastFifthDayMaxTemp;
+          weatherFiveDaysForecastFifthDayMinTemperature.textContent = weatherFiveDaysForecastFifthDayMinTemp;
+          weatherFiveDaysForecastFifthDayWindSpeed.textContent = weatherFiveDaysForecastFifthDayWindSpd;
+          weatherFiveDaysForecastFifthDayRainChance.textContent = (weatherFiveDaysForecastFifthDayRainCh*100);
 
           // Console logs for test
           console.log(data);
@@ -182,7 +254,7 @@ function convertUnixTimeStampToDay(timestamp) {
   let date = new Date(unix_timestamp * 1000);
   // Day part from the timestamp
   let day = date.getDay();
-  // Return the name of the weekday instead of a number
+  // Return the name of the day instead of a number
   let weekday = new Array(7);
   weekday[0] = "Sunday";
   weekday[1] = "Monday";
@@ -194,6 +266,23 @@ function convertUnixTimeStampToDay(timestamp) {
 
   // Display time in day name
   let formattedTime = weekday[day];
+  console.log(formattedTime);
+  return formattedTime;
+}
+
+// This function converts unix timestamps to a number date format (24/10)
+function convertUnixTimeStampToDayMonth(timestamp) {
+  let unix_timestamp = timestamp;
+  // Create a new JavaScript Date object based on the timestamp
+  // multiplied by 1000 so that the argument is in milliseconds, not seconds.
+  let date = new Date(unix_timestamp * 1000);
+  // Day of the month part from the timestamp
+  let day = date.getDate();
+  // Month part from the timestamp
+  let month = date.getMonth();
+
+  // Display time in day name
+  let formattedTime = (day + 1) + "/" + (month + 1);
   console.log(formattedTime);
   return formattedTime;
 }
